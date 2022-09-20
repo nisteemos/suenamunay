33f3079b839cde1e6e03195d8ff3d7cd40f73036
Nicolás Steeven Mosquera Gómez <34241505+nistemos@users.noreply.github.com>
2022-09-20 00:10:22 -0500

    -----

--- suenamunay-front/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  mapbox: {
-    accessToken: 'pk.eyJ1IjoibmlzdGVlbW9zIiwiYSI6ImNsMnh4enVxYjEwOHczY211ZDhlMDJsaHcifQ.DJGjh--Sp0d5nFmDBNViIg',
-  }
-};
